1e0941b62dd78d5fdb3d7b83309b33ff817d9652
CM.MartinoALMA <36303252+ciromart@users.noreply.github.com>
2019-04-01 14:34:17 +0200

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Survey, SurveyModel, SurveyNG, SurveyWindowNG, StylesManager } from '../assets/surveyjs/packages/survey-angular/survey.angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -9,32 +10,203 @@ import { Survey, SurveyModel, SurveyNG, SurveyWindowNG, StylesManager } from '..
 export class AppComponent implements OnInit {
   title = 'rationalproject';
 
-  constructor() { }
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+  }
 
   ngOnInit() {
 
-    var surveyJSON = {
-      title: "Tell us, what technologies do you use?", pages: [
-        {
-          name: "page1", questions: [
-            { type: "radiogroup", choices: ["Yes", "No"], isRequired: true, name: "frameworkUsing", title: "Do you use any front-end framework like Bootstrap?" },
-            { type: "checkbox", choices: ["Bootstrap", "Foundation"], hasOther: true, isRequired: true, name: "framework", title: "What front-end framework do you use?", visibleIf: "{frameworkUsing} = 'Yes'" }
+    var surveyJSON ={
+      "locale": "it",
+      "focusFirstQuestionAutomatic": false,
+      "pages": [
+       {
+        "name": "Pagina 1 ",
+        "elements": [
+         {
+          "type": "text",
+          "name": "question1",
+          "title": {
+           "it": "Nome Prova"
+          },
+          "isRequired": true,
+          "placeHolder": {
+           "it": "Inserisci Nome"
+          }
+         },
+         {
+          "type": "text",
+          "name": "question2",
+          "title": {
+           "it": "Data Nascita"
+          },
+          "isRequired": true,
+          "inputType": "date"
+         },
+         {
+          "type": "checkbox",
+          "name": "question3",
+          "title": {
+           "it": "Scegli una risposta: "
+          },
+          "isRequired": true,
+          "choices": [
+           {
+            "value": "item1",
+            "text": {
+             "it": "Angular 2+"
+            }
+           },
+           {
+            "value": "item2",
+            "text": {
+             "it": "ReactJs"
+            }
+           },
+           {
+            "value": "item3",
+            "text": {
+             "it": "JavaScript"
+            }
+           }
+          ],
+          "choicesOrder": "asc"
+         },
+         {
+          "type": "boolean",
+          "name": "question4",
+          "title": {
+           "it": "Laureato?"
+          }
+         },
+         {
+          "type": "file",
+          "name": "question5",
+          "title": {
+           "it": "Inserisci Carta D'Identità"
+          },
+          "isRequired": true,
+          "showPreview": false,
+          "storeDataAsText": false,
+          "maxSize": 0
+         },
+         {
+          "type": "radiogroup",
+          "name": "question6",
+          "title": {
+           "it": "Scegli ambiente"
+          },
+          "isRequired": true,
+          "choices": [
+           {
+            "value": "item1",
+            "text": {
+             "it": "Spring Boot"
+            }
+           },
+           {
+            "value": "item2",
+            "text": {
+             "it": "Eclipse"
+            }
+           }
+          ],
+          "choicesOrder": "random"
+         }
+        ],
+        "title": "Sezione1",
+        "questionsOrder": "initial"
+       },
+       {
+        "name": "Pagina 2",
+        "elements": [
+         {
+          "type": "text",
+          "name": "question7",
+          "title": {
+           "it": "Inserisci email"
+          },
+          "isRequired": true,
+          "validators": [
+           {
+            "type": "email"
+           }
+          ],
+          "inputType": "email",
+          "placeHolder": {
+           "it": "Email"
+          }
+         },
+         {
+          "type": "text",
+          "name": "question8",
+          "title": {
+           "it": "Inserisci Password"
+          }
+         },
+         {
+          "type": "text",
+          "name": "question9",
+          "title": {
+           "it": "Conferma Password"
+          }
+         },
+         {
+          "type": "dropdown",
+          "name": "question10",
+          "title": {
+           "it": "Età"
+          },
+          "choices": [
+           {
+            "value": "item1",
+            "text": {
+             "it": "Minore 18"
+            }
+           },
+           {
+            "value": "item2",
+            "text": {
+             "it": "Over 30"
+            }
+           },
+           {
+            "value": "item3",
+            "text": {
+             "it": "Over 60"
+            }
+           }
           ]
-        },
-        {
-          name: "page2", questions: [
-            { type: "radiogroup", choices: ["Yes", "No"], isRequired: true, name: "mvvmUsing", title: "Do you use any MVVM framework?" },
-            { type: "checkbox", choices: ["AngularJS", "KnockoutJS", "React"], hasOther: true, isRequired: true, name: "mvvm", title: "What MVVM framework do you use?", visibleIf: "{mvvmUsing} = 'Yes'" }]
-        },
-        {
-          name: "page3", questions: [
-            { type: "comment", name: "about", title: "Please tell us about your main requirements for Survey library" }]
-        }
-      ]
-    };
-    StylesManager.applyTheme("default")
+         }
+        ]
+       }
+      ],
+      "triggers": [
+       {
+        "type": "complete"
+       }
+      ],
+      "sendResultOnPageNext": true,
+      "showPageNumbers": true,
+      "clearInvisibleValues": "none",
+      "pagePrevText": {
+       "it": "indietro"
+      },
+      "pageNextText": {
+       "it": "avanti"
+      },
+      "completeText": {
+       "it": "finisci"
+      }
+     };
+    StylesManager.applyTheme("winterstone");
     var survey = new Survey(surveyJSON);
     //survey.onComplete.add(sendDataToServer);
     SurveyNG.render("surveyElement", { model: survey });
   }
+  switchLanguage(language: string) {
+    this.translate.use(language).subscribe(resp =>{
+      console.log('resp', resp);
+    });
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+//thirdy lsupport library
+import {TranslateLoader, TranslateModule, TranslateCompiler} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateMessageFormatCompiler} from 'ngx-translate-messageformat-compiler';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      },
+      compiler: {
+        provide: TranslateCompiler,
+        useClass: TranslateMessageFormatCompiler
+    }
+  })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
