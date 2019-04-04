3b452c785cf0d2388b4501242733c58ee4b48585
CM.MartinoALMA <36303252+ciromart@users.noreply.github.com>
2019-04-04 16:13:37 +0200

    fifth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 
 //thirdy lsupport library
@@ -11,28 +12,35 @@ import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {TranslateMessageFormatCompiler} from 'ngx-translate-messageformat-compiler';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
+import { LoginComponent } from './login/login.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { DettaglioVerificheComponent } from './dettaglio-verifiche/dettaglio-verifiche.component';
+import { RouterModule } from '@angular/router';
+import { ProfiloComponent } from './profilo/profilo.component';
 
-    TranslateModule.forRoot({
-      loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-      },
-      compiler: {
-        provide: TranslateCompiler,
-        useClass: TranslateMessageFormatCompiler
-    }
-  })
-  ],
-  providers: [],
+@NgModule({
+   declarations: [
+      AppComponent,
+      LoginComponent,
+      WelcomePageComponent,
+      DettaglioVerificheComponent,
+      ProfiloComponent
+   ],
+   imports: [
+      FormsModule,
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
+      RouterModule,
+      TranslateModule.forRoot({
+         loader: {
+             provide: TranslateLoader,
+             useFactory: HttpLoaderFactory,
+             deps: [HttpClient]
+         }
+     })
+ ],
+ providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
